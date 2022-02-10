c31e98065e378b1647d919b080e23b9675457765
eric fisher <ericf94@outlook.com>
2022-02-10 08:29:54 -0500

    Comments added

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,3 +3,14 @@ Names: Maria Skibinski, Eric Fisher
 Student IDs: *********, 100481944
 Date Completed: February 10th, 2022
 */
+
+/*All the text (body copy) for your site content above should be injected in the page via
+JavaScript only. For this Lab, you may hard code your text in string variables.*/
+
+
+/*Using only JavaScript change the Products link found in the Navbar above to Projects. */
+
+/*Using only JavaScript, add another link to the Navbar above named Human Resources,
+that sits between About Us and Contact Us. You may not hard code this in the html file
+this must be done using DOM manipulation. Ensure that you also include an appropriate
+font-icon using Font-Awesome */
