e2e11b7e1bcbfe454f14e6d2a990ad910359cde7
SkibbsMia <78924603+SkibbsMia@users.noreply.github.com>
2022-03-24 19:23:58 -0400

    changing activelink callback

--- Scripts/app.js
+++ Scripts/app.js
@@ -437,6 +437,7 @@
         case "edit": return displayEdit;
         case "login": return displayLogin;
         case "register": return displayRegister;
+        case "taskList": return DisplayTaskList;
         case "404": return display404;
         default:
           console.error("ERROR: callback does not exist: " + activeLink);
